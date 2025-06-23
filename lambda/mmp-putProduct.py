09b68490330371e833c50f197077a7b47095f982
sami-halwani1 <sami.halwani1@gmail.com>
2025-06-22 21:34:14 -0700

    PutProduct Logic Added
    Logic to handle PutProduct into DB table -- `mmp-db`.products added
    -Adds new product
    -Handles duplicate products
    
    Added some common DB commands that may be used

--- lambda/mmp-putProduct.py
+++ lambda/mmp-putProduct.py
@@ -13,12 +13,12 @@ def getVendorDetails(event):
         raise ValueError("One or more RDS environment variables are not set")
     
     sql = f"""
-    SELECT * FROM `{database_name}`.vendors WHERE vendor_name = :vendor_name
+    SELECT * FROM `{database_name}`.vendors WHERE id = :id
     """
     parameters = [
         {
-            'name': 'vendor_name',
-            'value': {'stringValue': event["vendor_data"]['vendor_name']}
+            'name': 'id',
+            'value': {'stringValue': event["vendor_data"]['id']}
         }
     ]
 
@@ -32,26 +32,70 @@ def getVendorDetails(event):
 
     return response['records'][0][0]['stringValue']
 
-def add_db_record(vendor_data):
+def build_rds_parameters(data: dict, alias_map: dict = None):
+    parameters = []
+
+    for key, value in data.items():
+        aliases = alias_map[key] if alias_map and key in alias_map else [key]
+
+        for alias in aliases:
+            param = {'name': alias}
+
+            if value is None:
+                param['value'] = {'isNull': True}
+            elif isinstance(value, str):
+                param['value'] = {'stringValue': value}
+            elif isinstance(value, int):
+                param['value'] = {'longValue': value}
+            elif isinstance(value, float):
+                param['value'] = {'doubleValue': value}
+            elif isinstance(value, bool):
+                param['value'] = {'booleanValue': value}
+            else:
+                raise ValueError(f"Unsupported value type for {key}")
+
+            parameters.append(param)
+
+    return parameters
+
+def add_db_record(vendor_id, product_data):
     rds = boto3.client('rds-data', region_name=os.environ.get('AWS_REGION'))
     database_name = os.environ.get('DB_NAME')
     cluster_arn = os.environ.get('DB_CLUSTER_ARN')
     secret_arn = os.environ.get('DB_SECRET_ARN')
     if not all([database_name, cluster_arn, secret_arn]):
         raise ValueError("One or more RDS environment variables are not set")
-    sql = f"""
-    INSERT INTO `{database_name}`.products ()
-    """
-    parameters = []
     
-    response = rds.execute_statement(
-        resourceArn=cluster_arn,
-        secretArn=secret_arn,
-        database=database_name,
-        sql=sql,
-        parameters=parameters
+    sql = f"""
+        INSERT INTO `{database_name}`.products (vendor_id, description, sku, name, price)
+        SELECT * FROM (
+            SELECT :vendor_id_insert, :description, :sku_insert, :name, :price
+        ) AS tmp
+        WHERE NOT EXISTS (
+            SELECT 1 FROM `{database_name}`.products
+            WHERE sku = :sku_check AND vendor_id = :vendor_id_check
+        ) LIMIT 1;
+        """
+    product_alias_map = {
+    'sku': ['sku_insert', 'sku_check'],
+    'name': ['name'],
+    'description': ['description'],
+    'price': ['price'],
+    'stock_quantity': ['stock_quantity'],
+    'vendor_id': ['vendor_id_insert', 'vendor_id_check']
+    }
+
+    for product in product_data['products']:
+        product_params = build_rds_parameters(product, product_alias_map)
+        rds.execute_statement(
+            resourceArn=cluster_arn,
+            secretArn=secret_arn,
+            database=database_name,
+            sql=sql,
+            parameters=product_params
     )
-    return response
+
+
 
 
 def connect_to_db():
